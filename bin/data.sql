81ac6cd871af783c9eb325557a64caea6afaf701
venkatesh-naik <venkatesh02@yahoo.com>
2021-05-14 16:38:04 +0530

    Added GitController to BranchX

---
+++ bin/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO 
+	TBL_EMPLOYEES (first_name, last_name, email) 
+VALUES
+  	('Lokesh', 'Gupta', 'howtodoinjava@gmail.com'),
+  	('John', 'Doe', 'xyz@email.com'),
+  	('mohan', 'Doe', 'mh@email.com'),
+  	('roopa', 'Doe', 'rp@email.com');
+  	
---
+++ src/main/java/com/example/infy/controller/GitController.java
@@ -0,0 +1,9 @@
+package com.example.infy.controller;
+
+public class GitController {
+	private void sysout() {
+System.out.println("added git controller");
+
+	}
+
+}
